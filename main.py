8f1bebdbe611ad700eb550ccebe56b4bb5406b6f
Nemo1166 <zhangguoxi1166@gmail.com>
2024-04-22 22:45:43 +0100

    bugfix

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def checker(root, path, logger):
 if __name__ == '__main__':
     logger = Logger('./text_check.log')
     proj_root = "../aneot/docs/posts/"
-    issues = [item for item in os.listdir(proj_root) if item != 'README.md']
+    issues = [item for item in os.listdir(proj_root) if item not in ['README.md', '.DS_Store']]
     for issue in issues:
         articles = [i for i in os.listdir(os.path.join(proj_root, issue)) if 'article' in i]
         for article in articles:
